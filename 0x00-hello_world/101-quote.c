26a46b447b2664141caeea14ac85966c15ca9d7b
Christelle57 <mbouteuchristelle@gmail.com>
2022-06-30 12:55:18 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,4 +1,6 @@
 #include <stdio.h>
+#include <unistd.h>
+
 /**
 * main - prints the size of various types
 * Return: 0 if exited properly, non-zero otherwise
@@ -8,3 +10,4 @@ int main(void)
 write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 return (1);
 }
+
