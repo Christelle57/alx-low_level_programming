c3dd1ea46a6b22c8f51eb0f332df36c0f40ccf64
Christelle Arielle <107045318+Christelle57@users.noreply.github.com>
2022-07-01 15:08:10 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,13 +18,9 @@ n = rand() - RAND_MAX / 2;
 scanf("%d", &n);
 
 if (n > 0)
-{
 printf("%d is positive\n", n);
-}
 if (n == 0)
-{
 printf("%d is zero\n", n);
-}
 if (n < 0)
 {
 printf("%d is negative\n", n);
