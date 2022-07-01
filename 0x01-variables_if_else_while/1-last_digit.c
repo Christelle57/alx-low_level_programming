86b42f64bf800c767fc11ac3bb41b8c1fb6929d0
Christelle57 <mbouteuchristelle@gmail.com>
2022-07-01 08:01:24 -0700

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -19,7 +19,6 @@ scanf("%d", &n);
 
 if (n > 5)
 {
-<<<<<<< HEAD
 printf("greater than 5\n");
 }
 if (n == 0)
@@ -31,20 +30,6 @@ if (n < 6 && n != 0)
 printf("less than 6 not 0\n");
 }
 
-=======
-printf("greater than 5");
-}
-if (n == 0)
-{
-printf("0");
-}
-if (n < 6 && n != 0)
-{
-printf("less than 6 not 0");
-}
-Print("\n");
-  
->>>>>>> 891acc414451a8bd104d38255142a6c7fb851cfd
 return (0);
 
 }
